33c39774cd2061c07ca33c4e95f027adc84f7b5c
DavidKlant <32843275+DavidKlant@users.noreply.github.com>
2017-10-20 11:04:04 +0200

    Update Programm.java

--- Uebung02/src/Programm.java
+++ Uebung02/src/Programm.java
@@ -1,2 +1,22 @@
-public class Programm {
+public class Programm{
+
+	public static void QuickSort(int[] feld) {
+		
+		for(int i=0; i<feld.length-1; ++i) {			//bubble sort
+			for(int j = i+1; j<feld.length; ++j) {
+				if(feld[i]>feld[j]) {
+					int temp = feld[i];		//swap
+					feld[i] = feld[j];
+					feld[j] = temp;
+				}
+			}
+		}	
+	}
+
+	
+	public static int IndexVonMitte(int[] feld, int i, int j) {
+		return (i+j+1)/2;
+	}
+	
+
 }
